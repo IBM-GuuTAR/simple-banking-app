4e70317e59001eeb83b162dc7d2bc885a16fea0d
Tinnapop-Pratheep <tinnapop.pratheep@ibm.com>
2026-01-29 17:29:56 +0700

    Add dependency

---
+++ website/src/providers/RootProvider.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from '@mui/material'
+import { ReactNode } from 'react'
+
+import { theme } from '@/theme/theme'
+
+type Props = {
+  children: ReactNode
+}
+
+export default function RootProvider({ children }: Props) {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
---
+++ website/src/theme/palette.ts
@@ -0,0 +1,27 @@
+import { PaletteOptions } from '@mui/material'
+
+export const PALETTE: PaletteOptions = {
+  primary: {
+    main: '#27187E',
+  },
+  secondary: {
+    main: '#758BFD',
+  },
+  text: {
+    primary: '#000',
+    secondary: '#FFF',
+  },
+  background: {
+    default: '#D9D9D9',
+    paper: '#FFF',
+  },
+  success: {
+    main: '#7BC950',
+  },
+  error: {
+    main: '#FF4000',
+  },
+  info: {
+    main: '#000',
+  },
+}
