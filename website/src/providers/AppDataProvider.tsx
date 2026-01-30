47ffbe299bbe8a76ea02b128a4b92cc6cf9caa09
Tinnapop-Pratheep <tinnapop.pratheep@ibm.com>
2026-01-30 17:48:31 +0700

    Integrate API

--- website/src/providers/AppDataProvider.tsx
+++ website/src/providers/AppDataProvider.tsx
@@ -1,16 +1,15 @@
-import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from 'react'
+import { createContext, ReactNode, useCallback, useContext, useEffect, useMemo, useState } from 'react'
 
-import { AccountData, TransactionData } from '@/types'
-
-import { TRANSACTIONS_DATA_MOCK } from '@/data'
+import { AccountData, DisplayAccountData, TransactionData, TransactionInput } from '@/types'
 
 import { accountService } from '@/service/AccountService'
+import { transactionService } from '@/service/TransactionService'
 
 export type AppDataContextType = {
-  selectedAccount?: AccountData
-  selectUser: (account: AccountData) => void
-  accounts: AccountData[]
-  transactions: TransactionData[]
+  selectedAccount?: DisplayAccountData
+  selectUser: (accountId: number) => void
+  displayAccounts: DisplayAccountData[]
+  insertTransaction: (transactionInput: TransactionInput) => void
   isDataLoaded: boolean
 }
 
@@ -23,28 +22,50 @@ type Props = {
 export const AppDataProvider = ({ children }: Props) => {
   const [isDataLoaded, setIsDataLoaded] = useState<boolean>(false)
 
-  const [selectedAccount, setSelectedAccount] = useState<AccountData>()
+  const [displayAccounts, setDisplayAccounts] = useState<DisplayAccountData[]>([])
+  const [selectedAccountId, setSelectedAccountId] = useState<number>()
 
-  const [accounts, setAccounts] = useState<AccountData[]>([])
-  const [transactions, setTransactions] = useState<TransactionData[]>([])
+  const selectedAccount: DisplayAccountData | undefined = useMemo(
+    () => displayAccounts.find((account) => account.id === selectedAccountId),
+    [displayAccounts, selectedAccountId],
+  )
 
   useEffect(() => {
     const initialize = async () => {
-      const _accounts = await accountService.fetchAccount()
-      setAccounts(_accounts)
-      setTransactions(TRANSACTIONS_DATA_MOCK)
+      const _accounts: AccountData[] = await accountService.fetchAccount()
+      const _transactions: TransactionData[] = await transactionService.fetchTransaction()
+
+      const displayAccounts: DisplayAccountData[] = accountService.renderDisplayAccounts(_accounts, _transactions)
+      setDisplayAccounts(displayAccounts)
+
       setIsDataLoaded(true)
     }
 
     initialize()
   }, [])
 
-  const selectUser = useCallback((account: AccountData) => {
-    setSelectedAccount(account)
+  const fetchData = useCallback(async () => {
+    const _accounts: AccountData[] = await accountService.fetchAccount()
+    const _transactions: TransactionData[] = await transactionService.fetchTransaction()
+
+    const displayAccounts: DisplayAccountData[] = accountService.renderDisplayAccounts(_accounts, _transactions)
+    setDisplayAccounts(displayAccounts)
   }, [])
 
+  const selectUser = useCallback((accountId: number) => {
+    setSelectedAccountId(accountId)
+  }, [])
+
+  const insertTransaction = useCallback(
+    async (transactionInput: TransactionInput) => {
+      await transactionService.insertTransaction(transactionInput)
+      fetchData()
+    },
+    [fetchData],
+  )
+
   return (
-    <AppDataContext.Provider value={{ selectedAccount, selectUser, accounts, transactions, isDataLoaded }}>
+    <AppDataContext.Provider value={{ selectedAccount, selectUser, displayAccounts, insertTransaction, isDataLoaded }}>
       {children}
     </AppDataContext.Provider>
   )
