09ddf51f4bfc6f8e7964546f389a8300018e14bf
Tinnapop-Pratheep <tinnapop.pratheep@ibm.com>
2026-01-30 12:29:10 +0700

    Init Account API with mock data

---
+++ backend/src/main/java/com/guutar/simplebankingapp/repository/AccountRepository.java
@@ -0,0 +1,24 @@
+package com.guutar.simplebankingapp.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.guutar.simplebankingapp.model.Account;
+
+@Repository
+public class AccountRepository {
+    private final List<Account> accounts;
+
+    public AccountRepository() {
+        this.accounts = new ArrayList<>();
+        accounts.add(new Account(0, "Tony Stark", 100_000_000));
+        accounts.add(new Account(1, "Peter Parker", 0));
+        accounts.add(new Account(1, "Tar Richman", 500));
+    }
+
+    public List<Account> findAll() {
+        return this.accounts;
+    }
+}
